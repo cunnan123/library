e97b51f7438cbed448760937d3426a2fcff9cb5f
John Doe <johndoe@example.com>
2019-11-01 16:37:35 +0800

    上传platform
    Signed-off-by: John Doe <johndoe@example.com>

--- 平台搭建/platform2/unit/index.unit.js
+++ 平台搭建/platform2/unit/index.unit.js
@@ -1,9 +1,19 @@
-
 describe("index", function () {
     it("add", function () {
-        chai.assert.equal(add(2,4),6)
+        chai.assert.equal(add(2, 4), 6)
     });
     it("add2", function () {
-        chai.assert.equal(add2(2,4),6)
+        chai.assert.equal(add2(2, 4), 6)
     });
+
+    it('add3', function () {
+        var foo = 'bar'
+        var beverages = {
+            tea: ['chai', 'matcha', 'oolong']
+        }
+        chai.expect(foo).to.be.a('string');
+        chai.expect(foo).to.equal('bar');
+        chai.expect(foo).to.have.lengthOf(3);
+        chai.expect(beverages).to.have.property('tea').with.lengthOf(3);
+    })
 });
