58d391468a08a1ed5762c9b6b168a17d185bf859
John Doe <johndoe@example.com>
2019-12-17 18:46:10 +0800

    上传是scss Signed-off-by: John Doe <johndoe@example.com>

---
+++ vue/demo3/src/app.vue
@@ -0,0 +1,22 @@
+<template><div class="example">
+  <div> {{msg}}</div>
+<img src="./logo.png">
+</div>
+</template>
+<script>export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+
+</script>
+<style lang="scss">
+  $primary:red;
+  .example{
+   color: $primary;
+  }
+ 
+
+</style>
---
+++ vue/demo3/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+     
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+     
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 20000,
+              // limit: 200,
+              esModule :false
+            },
+          }
+        ],
+      }   
+    ]
+  },
+  plugins: [
+   
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
+}
