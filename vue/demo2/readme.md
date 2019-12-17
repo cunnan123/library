034a9b27f8c66864b8802fdc5f146a14fe6b6124
John Doe <johndoe@example.com>
2019-12-17 10:34:17 +0800

    上传vue cli demo Signed-off-by: John Doe <johndoe@example.com>

---
+++ vue/demo2/readme.md
@@ -0,0 +1,16 @@
+初始化项目
+    npm init -y
+安装webpack
+   npm install --save-dev webpack webpack-cli
+新建webpack.config.js .gitignore
+安装vue 
+    npm install --save-dev vue vue-loader vue-template-compiler
+配置webpack.config.js
+新建src index.js app.vue 
+安装html-webpack-plugin
+    npm install --save-dev html-webpack-plugin
+安装babel-loader  vue-style-loader css-loader
+npm install --save-dev babel-loader  vue-style-loader css-loader
+安装@babel/core
+npm install --save-dev @babel/core
+运行npm run build
---
+++ vue/demo2/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+     
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+     
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+   
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
+}
