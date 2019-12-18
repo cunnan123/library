90289a531f397f126aa15e08241fb040c89546f7
John Doe <johndoe@example.com>
2019-12-18 15:49:13 +0800

    CSS 提取 Signed-off-by: John Doe <johndoe@example.com>

---
+++ vue/demo10/src/app.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+<div>
+  <div :class="[$style.red, $style.bold]">hello world</div>
+</div>
+</template>
+<script>
+export default {
+   created () {
+    console.log(this.$style)
+    // -> "red_1VyoJ-uZ"
+    // 一个基于文件名和类名生成的标识符
+  }
+}
+
+</script>
+<style lang="scss" scoped module>
+ $primary:blue;
+  .red{
+   color: $color;
+  }
+  .bold{
+   background: $primary;
+  }
+</style>
