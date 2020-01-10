6a92aab1370fe9b34e16218da475e39c8ba457fe
John Doe <johndoe@example.com>
2020-01-10 18:02:08 +0800

    Signed-off-by: John Doe <johndoe@example.com>

--- vue/vue/demo3/my-project/src/main.js
+++ vue/vue/demo3/my-project/src/main.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 Vue.config.productionTip = false
-Vue.filter('capitalize', function (value) {
-  if (!value) return ''
-  value = value.toString()
-  return value.charAt(0).toUpperCase() + value.slice(1)
-})
 
 new Vue({
   el: '#app',
