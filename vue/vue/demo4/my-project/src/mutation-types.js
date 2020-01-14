ba123cd2b3d5c060dd50c40fe4d9af16ca3becbd
John Doe <johndoe@example.com>
2020-01-14 17:21:28 +0800

    Signed-off-by: John Doe <johndoe@example.com>

--- vue/vue/demo4/my-project/src/mutation-types.js
+++
@@ -1,2 +0,0 @@
-// 可以让你的代码合作者对整个 app 包含的 mutation 一目了然
-export const SOME_MUTATION = 'SOME_MUTATION'
--- 设计模式/ES6/demo1/src/strategypattern/library/RebateCashClass.js
+++
@@ -1,9 +0,0 @@
-class RebateCashClass extends CashClass{
-    constructor(rebate){
-        super();
-        this.rebate= Number(rebate);
-    }
-    acceptCash(money){
-        return money*this.rebate;
-    }
-}
