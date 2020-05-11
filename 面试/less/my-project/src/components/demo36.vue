56279578d68e4450eae47f8884fd01b36f9e1832
John Doe <johndoe@example.com>
2020-05-11 08:01:47 +0800

    Signed-off-by: John Doe <johndoe@example.com>

---
+++ 面试/less/my-project/src/components/demo36.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+     <div class="box" id="header">
+        <div>demo37</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'demo36',
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped lang='less'>
+@nice-blue: #5B83AD;
+@light-blue: @nice-blue + #111;
+
+#header {
+  color: @light-blue;
+}
+</style>
---
+++ 面试/less/my-project/src/components/demo37.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <div class="box">
+        <div>demo37</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'demo37',
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
