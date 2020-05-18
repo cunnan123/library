0ac240061a2ec3f0afb0cbcff436ef8ee731ecea
John Doe <johndoe@example.com>
2020-05-18 19:13:11 +0800

    Signed-off-by: John Doe <johndoe@example.com>

---
+++ 小程序/test/js/model/userInfoClass/baseUserInfoClass.js
@@ -0,0 +1,30 @@
+import userInfo from './userInfoClass';
+export default class baseUserInfoClass extends userInfo{
+  constructor(){
+    super();
+    this.name=null;//姓名
+    this.age=null;//年龄
+    this.sex=null;//性别
+    this.address=null;//地址
+    this.openid=null;//小程序openid
+    this.avatar=null;//头像
+    this.nickname=null;//昵称
+  }
+  //获取用户信息
+  getUserInfo(){  
+    return {
+      nanme:this.name||null,
+      age:this.age||null,
+      sex:this.sex||null,
+      address:this.address||null,
+      openid:this.openid||null,
+      avatar:this.avatar||null,
+      nickname:this.nickname||null,
+    }
+  }
+}
+
+// 测试
+var userA=new baseUserInfoClass()
+var info=userA.getUserInfo();
+console.log(info)
